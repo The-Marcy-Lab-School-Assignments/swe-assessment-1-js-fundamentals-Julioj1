1daaad05134340670c6c1c1b932d8911f1d55496
Julioj1 <dbjulioj@gmail.com>
2025-10-22 15:40:57 -0400

    Completed debug.js

--- src/debug.js
+++ src/debug.js
@@ -13,13 +13,13 @@
 
 const fixVariables = (temp) => {
   if (temp < 30) {
-    const msg = 'Pretty chilly.';
+    msg = 'Pretty chilly.';
   } else if (temp < 70) {
-    const msg = 'Not bad.';
+    msg = 'Not bad.';
   } else if (temp < 100) {
-    const msg = 'On the hot side.';
+    msg = 'On the hot side.';
   } else {
-    const msg = 'I will die of heat.';
+    msg = 'I will die of heat.';
   }
   console.log(msg);
   console.log("And that's how I feel about the temp!");
